f7b51f23a22d68ec41d9bf20d0f4da89b243147d
edwinbosco2 <edwinbosco2007@gmail.com>
2026-09-19 22:04:41 -0400

    product page

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,18 @@
-const menuBtn=document.querySelector(".menuBtn");
-const menuBar=document.querySelector(".menuBarCon");
+const menuBtn = document.querySelector(".menuBtn");
+const menuBar = document.querySelector(".menuBarCon");
+
 
 menuBtn.addEventListener('click',()=>{
-    if(menuBar.style.display == "none"){
-        menuBar.style.display="block";
+    const menubar=window.getComputedStyle(menuBar);
+    if(menubar.display === "none"){
+        menuBar.style.display = "block";
+    }else{
+        menuBar.style.display= "none";
+    }
+});
+
+document.addEventListener('click',(e)=>{
+    if(!menuBar.contains(e.target) && !menuBtn.contains(e.target)){
+        menuBar.style.display="none";
     }
 })
